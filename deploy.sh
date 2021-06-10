9d38edd86e3f215543651fddb8a032769fab35ca
renxiaojian <renxiaojian@hikvision.com.cn>
2021-06-10 12:38:52 +0800

    project

--- deploy.sh
+++ deploy.sh
@@ -10,21 +10,36 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
+# echo 'b.xugaoyi.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:renxiaojian/renxiaojian.github.io.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://renxiaojian:${GITHUB_TOKEN}@github.com/renxiaojian/renxiaojian.github.io.git
+  git config --global user.name "renxiaojian"
+  git config --global user.email "466814659@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl master # 推送到github gh-pages分支
+
+cd -
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='project'
+  githubUrl=git@github.com:renxiaojian/rblog.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://renxiaojian:${GITHUB_TOKEN}@github.com/renxiaojian/rblog.git
+  git config --global user.name "renxiaojian"
+  git config --global user.email "466814659@qq.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -47,41 +47,41 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'renxiaojian', // 必需
+    link: 'https://github.com/renxiaojian', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
     avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    name: 'Evan Xu',
-    slogan: '前端界的小学生',
+    name: 'jian',
+    slogan: '前端界的菜鸟',
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
     // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-    icons: [
-      {
-        iconClass: 'icon-youjian',
-        title: '发邮件',
-        link: 'mailto:894072666@qq.com',
-      },
-      {
-        iconClass: 'icon-github',
-        title: 'GitHub',
-        link: 'https://github.com/xugaoyi',
-      },
-      {
-        iconClass: 'icon-erji',
-        title: '听音乐',
-        link: 'https://music.163.com/#/playlist?id=755597173',
-      },
-    ],
+    // icons: [
+    //   {
+    //     iconClass: 'icon-youjian',
+    //     title: '发邮件',
+    //     link: 'mailto:894072666@qq.com',
+    //   },
+    //   {
+    //     iconClass: 'icon-github',
+    //     title: 'GitHub',
+    //     link: 'https://github.com/xugaoyi',
+    //   },
+    //   {
+    //     iconClass: 'icon-erji',
+    //     title: '听音乐',
+    //     link: 'https://music.163.com/#/playlist?id=755597173',
+    //   },
+    // ],
   },
   footer: {
     // 页脚信息
-    createYear: 2019, // 博客创建年份
-    copyrightInfo:
-      'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+    createYear: 2021, // 博客创建年份
+    // copyrightInfo:
+    //   'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
